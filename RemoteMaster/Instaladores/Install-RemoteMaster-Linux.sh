0cd0e5ffd9f0e7d2f197ac6a71c29419d7434933
Maikel-HA17 <5815957@alu.murciaeduca.es>
2025-02-05 17:35:55 +0100

    Add files via upload

--- RemoteMaster/Instaladores/Install-RemoteMaster-Linux.sh
+++ RemoteMaster/Instaladores/Install-RemoteMaster-Linux.sh
@@ -1,45 +1,15 @@
 #!/bin/bash
 
-# Nombre del archivo requirements.txt
-requirements="requirements.txt"
+# Ejecutar como administrador
 
-# Instalar python full
-apt install python-full -y
+if [ "$(id -u)" -eq 1 ]; then
+    echo "Debes ejecutar el script como administrador"
+    exit(1)
 
-# Función para instalar paquetes usando pip
-install_with_pip() {
-    echo "Instalando paquetes con pip"
-    python3 -m pip install -r "$requirements" --user
-}
+# Instalar OpenSSH server
+apt update
+apt install openssh-server
 
-# Función para instalar paquetes usando apt (si pip falla)
-install_with_apt() {
-    echo "Intentando instalar con apt"
-    while IFS= read -r package; do
-        # Asegúrate de que el paquete no esté vacío
-        if [[ ! -z "$package" ]]; then
-            echo "Instalando $package con apt..."
-            sudo apt-get install -y python3-"$package"
-        fi
-    done < "$requirements"
-}
-
-# Verificar si el archivo requirements.txt existe
-if [[ ! -f "$requirements" ]]; then
-    echo "El archivo $requirements no existe. Asegúrate de que esté presente."
-    exit 1
-fi
-
-# Intentar instalar con pip
-install_with_pip
-
-# Verificar si hubo error en la instalación con pip
-if [[ $? -ne 0 ]]; then
-    echo "Error al instalar con pip. Probando con apt..."
-    sleep 1
-    clear
-    install_with_apt
-    echo "Ya puedes usar RemoteMaster !!"
-else 
-    echo "Ya puedes usar RemoteMaster !!"
-fi
+# Iniciar e habilitar
+systemctl start sshd
+systemctl enable sshd
