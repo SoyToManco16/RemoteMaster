f231687a4d63e33e022a8646a7e48483280f1808
Maikel-HA17 <5815957@alu.murciaeduca.es>
2025-02-06 12:55:28 +0100

    Update Installer-Windows.ps1

--- RemoteMaster/Instaladores/Installer-Windows.ps1
+++ RemoteMaster/Instaladores/Installer-Windows.ps1
@@ -204,7 +204,7 @@ function enable_winrm_https {
 
 function install_python_dependencies {
     # Lista de librerías a instalar
-    $librerias = @("python-nmap", "psutil", "paramiko", "pyautogui", "colorama")
+    $librerias = @("python-nmap", "psutil", "paramiko", "pyautogui", "colorama", "pywinrm")
 
     foreach ($lib in $librerias) {
         try {
